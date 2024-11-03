210a6edc38b814f9a2c6b40b7970ff7901e96018
leon <leonstefanski@proton.me>
2024-11-03 19:31:47 +0100

    Adding and removing board cover image, some weak warnings fixed

--- src/components/AddBoardModal.jsx
+++ src/components/AddBoardModal.jsx
@@ -1,94 +1,165 @@
 import {useState} from "react";
 import axios from "axios";
-import {Box, Button, IconButton, Modal, Paper, Stack, TextField, Typography} from "@mui/material";
-import {Close, Upload} from "@mui/icons-material";
+import {Box, Button, Divider, Fab, IconButton, Modal, Paper, Stack, TextField, Typography} from "@mui/material";
+import {Close, Delete, Edit, Upload} from "@mui/icons-material";
 import PropTypes from "prop-types";
+import {useTheme} from "@mui/material/styles";
 
 const AddBoardModal = ({modalOpen, handleCloseModal}) => {
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
+  const theme = useTheme();
 
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
 
-    const addBoard = (name, userId) => {
-        axios.post("http://localhost:4000/boards", {
-            name: name,
-            owner: userId
-        })
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
+  const [file, setFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const addBoard = (name, userId) => {
+    axios.post("http://localhost:4000/boards", {
+      name: name,
+      owner: userId
+    })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const handleSave = () => {
+    addBoard(name, "67261b44afe3f93052c27a2e")
+    handleCloseModal()
+    window.location.reload();
+  }
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    if (selectedFile) {
+      setPreviewUrl(URL.createObjectURL(selectedFile)); // Generate preview URL
     }
+  };
 
-    const handleSave = () => {
-        addBoard(name, "67261b44afe3f93052c27a2e")
-        handleCloseModal()
-        window.location.reload();
-    }
+  const handleDeleteCoverImage = () => {
+    setPreviewUrl(null);
+    setFile(null)
+  }
 
-    return (
-        <Modal
-            open={modalOpen}
-            onClose={handleCloseModal}
-        >
-            <Paper sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                padding: 2,
-                minWidth: 350,
-                width: "90%",
-                borderRadius: 5,
-            }}>
-                <Stack spacing={1}>
-                    <Stack direction={'row'} justifyContent={'space-between'}>
-                        <IconButton onClick={handleCloseModal}>
-                            <Close/>
-                        </IconButton>
-                        <Typography>
-                            Create a Board
-                        </Typography>
-                        <Box width={'56px'}/>
-                    </Stack>
-                    <Button variant={'outlined'} sx={{height: 150}}>
-                        <Stack alignItems={'center'} justifyContent={'center'}>
-                            <Upload sx={{fontSize: 60}}/>
-                            <Typography>
-                                Upload a cover image
-                            </Typography>
-                        </Stack>
-                    </Button>
-                    <TextField
-                        fullWidth
-                        label="Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+  // TODO: UPLOAD IMAGE COVER + ADD TO API
+  // TODO: FORM VALIDATION
+
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={handleCloseModal}
+    >
+      <Paper sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        padding: 2,
+        minWidth: 350,
+        width: "90%",
+        borderRadius: 5,
+      }}>
+        <Stack spacing={1}>
+          <Stack direction='row' justifyContent={'space-between'}>
+            <IconButton onClick={handleCloseModal}>
+              <Close/>
+            </IconButton>
+            <Typography>
+              Create a Board
+            </Typography>
+            <Box width={'56px'}/>
+          </Stack>
+
+          <Button variant='outlined' sx={{height: 150, padding: 0}} fullWidth disableRipple component="span">
+            {previewUrl ?
+              <>
+                <img src={previewUrl} alt="cover image"
+                     style={{
+                       width: "100%",
+                       height: "100%",
+                       objectFit: "cover", objectPosition: "center"
+                     }}/>
+
+                <Fab color="primary" variant="extended" disableRipple
+                     style={{position: "absolute", bottom: 10, right: 10}}
+                >
+                  <Stack direction='row'>
+                    <label htmlFor="file-input">
+                      <IconButton sx={{pointerEvents: "none"}}>
+                        <Edit
+                          sx={{pointerEvents: "none", color: theme.palette.primary.darker}}/>
+                      </IconButton>
+                    </label>
+                    <Divider orientation="vertical" flexItem
+                             sx={{marginX: 1, marginY: 0.5, bgcolor: theme.palette.primary.darker}}
                     />
-                    <TextField
-                        fullWidth
-                        label="Description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
+                    <IconButton onClick={handleDeleteCoverImage}>
+                      <Delete sx={{color: theme.palette.primary.darker}}/>
+                    </IconButton>
+                  </Stack>
 
-                    <Stack width={'100%'}
-                           directon={'row'} alignItems={'flex-end'}>
-                        <Button variant={"contained"} sx={{maxWidth: 100}} onClick={handleSave}>
-                            Save
-                        </Button>
-                    </Stack>
+                </Fab>
+
+              </>
+              : <label htmlFor="file-input" style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}>
+                <Stack alignItems={'center'} justifyContent={'center'} sx={{padding: 1}}>
+                  <Upload sx={{fontSize: 60}}/>
+                  <Typography>
+                    Upload a cover image
+                  </Typography>
+                  {file && <p>Selected file: {file.name}</p>}
                 </Stack>
-            </Paper>
-        </Modal>
-    )
+              </label>}
+          </Button>
+
+
+          {/* File input outside of Button to avoid styling conflicts */}
+          <input
+            id="file-input"
+            type="file"
+            accept="image/*"
+            style={{display: 'none'}} // Hide the input visually
+            onChange={handleFileChange}
+          />
+          <TextField
+            fullWidth
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <Stack width={'100%'}
+                 directon={'row'} alignItems={'flex-end'}>
+            <Button variant="contained" sx={{maxWidth: 100}} onClick={handleSave}>
+              Save
+            </Button>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Modal>
+  )
 }
 
 AddBoardModal.propTypes = {
-    modalOpen: PropTypes.bool.isRequired,
-    handleCloseModal: PropTypes.func.isRequired,
+  modalOpen: PropTypes.bool.isRequired,
+  handleCloseModal: PropTypes.func.isRequired,
 };
 
 export default AddBoardModal
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,101 +7,101 @@ import React from "react";
 import {useNavigate} from "react-router-dom";
 
 export default function TopBar({header, root}) {
-    const theme = useTheme(); // Accessing theme to dynamically handle spacing
-    const navigate = useNavigate()
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const menuOpen = Boolean(anchorEl);
-    const handleClickMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleCloseMenu = () => {
-        setAnchorEl(null);
-    };
+  const theme = useTheme(); // Accessing theme to dynamically handle spacing
+  const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const menuOpen = Boolean(anchorEl);
+  const handleClickMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
 
-    const [modalOpen, setModalOpen] = React.useState(false);
-    const handleOpenModal = () => setModalOpen(true);
-    const handleCloseModal = () => setModalOpen(false);
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
 
-    // TODO: PROFILE MODAL
-    return (
-        <>
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={menuOpen}
-                onClose={handleCloseMenu}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-            >
-                <MenuItem onClick={handleCloseMenu}>Option 1</MenuItem>
-                <MenuItem onClick={handleCloseMenu}>Option 2</MenuItem>
-                <MenuItem onClick={handleCloseMenu}>Option 3</MenuItem>
-            </Menu>
-            <Modal
-                open={modalOpen}
-                onClose={handleCloseModal}
-            >
-                <Paper sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    padding: 2,
-                    minWidth: 350,
-                    borderRadius: 5,
-                    bgcolor: "primary.darker"
-                }}>
-                    <Stack direction={"row"} justifyContent={"space-between"}>
-                        <IconButton>
-                            <Close/>
-                        </IconButton>
-                        <Typography>
-                            Profile
-                        </Typography>
-                        <Box
-                            width={"56px"}/>
-                    </Stack>
+  // TODO: PROFILE MODAL
+  return (
+    <>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={menuOpen}
+        onClose={handleCloseMenu}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleCloseMenu}>Option 1</MenuItem>
+        <MenuItem onClick={handleCloseMenu}>Option 2</MenuItem>
+        <MenuItem onClick={handleCloseMenu}>Option 3</MenuItem>
+      </Menu>
+      <Modal
+        open={modalOpen}
+        onClose={handleCloseModal}
+      >
+        <Paper sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          padding: 2,
+          minWidth: 350,
+          borderRadius: 5,
+          bgcolor: "primary.darker"
+        }}>
+          <Stack direction="row" justifyContent={"space-between"}>
+            <IconButton>
+              <Close/>
+            </IconButton>
+            <Typography>
+              Profile
+            </Typography>
+            <Box
+              width={"56px"}/>
+          </Stack>
 
-                    <Paper sx={{padding: 2}}>
-                        <Stack direction={"row"}>
-                            <Avatar sx={{bgcolor: deepOrange[500]}}>
-                                N
-                            </Avatar>
-                            <Typography>
-                                Name
-                            </Typography>
-                        </Stack>
-                    </Paper>
-                </Paper>
-            </Modal>
-            <AppBar elevation={0} position="fixed">
-                <Toolbar>
-                    <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
-                        {root ?
-                            <IconButton size="large" edge="start" sx={{mr: 2}} onClick={handleClickMenu}>
-                                <MoreVert/>
-                            </IconButton> :
-                            <IconButton size="large" edge="start" sx={{mr: 2}} onClick={() => {
-                                navigate(-1)
-                            }}>
-                                <ArrowBack/>
-                            </IconButton>}
+          <Paper sx={{padding: 2}}>
+            <Stack direction="row">
+              <Avatar sx={{bgcolor: deepOrange[500]}}>
+                N
+              </Avatar>
+              <Typography>
+                Name
+              </Typography>
+            </Stack>
+          </Paper>
+        </Paper>
+      </Modal>
+      <AppBar elevation={0} position="fixed">
+        <Toolbar>
+          <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
+            {root ?
+              <IconButton size="large" edge="start" sx={{mr: 2}} onClick={handleClickMenu}>
+                <MoreVert/>
+              </IconButton> :
+              <IconButton size="large" edge="start" sx={{mr: 2}} onClick={() => {
+                navigate(-1)
+              }}>
+                <ArrowBack/>
+              </IconButton>}
 
-                        <Typography variant="h6" component="div">
-                            {header}
-                        </Typography>
-                        <Avatar sx={{bgcolor: deepOrange[500]}} onClick={handleOpenModal}>N</Avatar>
-                    </Stack>
-                </Toolbar>
-            </AppBar>
+            <Typography variant="h6" component="div">
+              {header}
+            </Typography>
+            <Avatar sx={{bgcolor: deepOrange[500]}} onClick={handleOpenModal}>N</Avatar>
+          </Stack>
+        </Toolbar>
+      </AppBar>
 
-            <div style={{height: theme.mixins.toolbar.minHeight}}/>
-        </>
-    );
+      <div style={{height: theme.mixins.toolbar.minHeight}}/>
+    </>
+  );
 }
 
 TopBar.propTypes = {
-    header: PropTypes.string.isRequired,
-    root: PropTypes.bool,
+  header: PropTypes.string.isRequired,
+  root: PropTypes.bool,
 };
